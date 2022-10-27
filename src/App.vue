17577aff14f66bda6160012076bd5d41f571472f
ferv3455 <sh20070228@163.com>
2022-10-27 16:42:15 +0800

    add element-ui dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+
+const count = ref(0);
+
+function increment() {
+  count.value++
+}
 </script>
 
 <template>
@@ -9,6 +16,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/login">Login</RouterLink>
       <RouterLink to="/register">Register</RouterLink>
+      <el-button type="primary" @click="increment()">(By Element UI) Count is: {{ count }}</el-button>
     </nav>
   </header>
 
